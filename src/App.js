68d455bba307a0a034c250c2bcd70cbeaa81c711
Md. Manwar Hossain <baquib144@gmail.com>
2023-02-08 16:10:57 +0530

    Builded Main Landing Page

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import Header from './components/Header';
 import About from './components/About';
 import Workouts from './components/Workouts';
 import Pricing from './components/Pricing';
-import Community from './components/Community';
-import Faq from './components/Faq';
 import Join from './components/Join';
 import Footer from './components/Footer';
 
 const App = () => {
   // aos initialization
+  Aos.init({
+    duration: 2500,
+    delay: 400,
+  });
   return (
     <div className='max-w-[1440px] mx-auto bg-page overflow-hidden relative'>
       <Header />
@@ -24,8 +26,6 @@ const App = () => {
       <About />
       <Workouts />
       <Pricing />
-      <Community />
-      <Faq />
       <Join />
       <Footer />
       {/* <div className='h-[4000px]'></div> */}
--- src/data.js
+++ src/data.js
@@ -1,36 +1,136 @@
-import React from 'react';
+// images
+import Logo from '../src/assets/img/header/logo.svg';
+import ResistanceImg from '../src/assets/img/workouts/resistance.png';
+import BoxingImg from '../src/assets/img/workouts/boxing.png';
+import BodyPumpImg from '../src/assets/img/workouts/body-pump.png';
+import YogaImg from '../src/assets/img/workouts/yoga.png';
+import FullBodyImg from '../src/assets/img/workouts/full-body.png';
+import FitnessImg from '../src/assets/img/workouts/fitness.png';
+import BattleRopeImg from '../src/assets/img/workouts/battle-rope.png';
+import JoinImg from '../src/assets/img/join/woman.png';
+// icons
+import UsersIcn from '../src/assets/img/about/icons/users-icn.svg';
+import CalendarIcn from '../src/assets/img/workouts/icons/calendar.svg';
+import PriceIcn from '../src/assets/img/pricing/icons/price.svg';
 
-// import aos
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-
-// import components
-import Banner from './components/Banner';
-import Header from './components/Header';
-import About from './components/About';
-import Workouts from './components/Workouts';
-import Pricing from './components/Pricing';
-import Community from './components/Community';
-import Faq from './components/Faq';
-import Join from './components/Join';
-import Footer from './components/Footer';
-
-const App = () => {
-  // aos initialization
-  return (
-    <div className='max-w-[1440px] mx-auto bg-page overflow-hidden relative'>
-      <Header />
-      <Banner />
-      <About />
-      <Workouts />
-      <Pricing />
-      <Community />
-      <Faq />
-      <Join />
-      <Footer />
-      {/* <div className='h-[4000px]'></div> */}
-    </div>
-  );
+export const header = {
+  logo: Logo,
+  btnLoginText: 'Log in',
+  btnSignupText: 'Sign Up',
 };
 
-export default App;
+export const nav = [
+  { name: 'Home', href: '/' },
+  { name: 'About', href: '/' },
+  { name: 'Workouts', href: '/' },
+  { name: 'Pricing', href: '/' },
+  { name: 'Contact Us', href: '/' },
+  { name: 'Review', href: '/' },
+];
+
+export const banner = {
+  titlePart1: 'Fit in and make today your best',
+  titlePart2: '– join here stay fit.',
+  subtitle:
+    'The pain you feel today will be the strength you feel tomorrow. Keep pushing.',
+  textBtn: 'Join Now',
+  img: '',
+};
+
+export const about = {
+  icon: UsersIcn,
+  title: 'Our mission',
+  subtitle1:
+    'At our gym, we believe in the power of inspiration to transform lives. With a motivating atmosphere, expert staff, and state-of-the-art equipment, we provide everything our members need to reach their fitness goals..',
+  subtitle2:
+    'We are committed to empowering each person who walks through our doors to become their best selves.',
+  link: 'Join Now',
+};
+
+export const workouts = {
+  icon: CalendarIcn,
+  title: 'Training programs',
+  programs: [
+    {
+      image: ResistanceImg,
+      name: 'Resistance',
+    },
+    {
+      image: BoxingImg,
+      name: 'Boxing',
+    },
+    {
+      image: BodyPumpImg,
+      name: 'Body Pump',
+    },
+    {
+      image: YogaImg,
+      name: 'Yoga',
+    },
+    {
+      image: FullBodyImg,
+      name: 'Full Body',
+    },
+    {
+      image: FitnessImg,
+      name: 'Fitness',
+    },
+    {
+      image: BattleRopeImg,
+      name: 'Battle Rope',
+    },
+  ],
+};
+
+export const pricing = {
+  icon: PriceIcn,
+  title: 'Pricing plan',
+  plans: [
+    {
+      name: 'Basic',
+      price: '500',
+      list: [
+        { name: 'unlimited gym access' },
+        { name: '1 training programs' },
+        { name: 'free fitness consultation' },
+      ],
+      delay: 600,
+    },
+    {
+      name: 'Premium',
+      price: '1000',
+      list: [
+        { name: 'unlimited gym access' },
+        { name: '5 training programs' },
+        { name: 'free fitness consultation' },
+        { name: 'personal trainer' },
+      ],
+      delay: 800,
+    },
+    {
+      name: 'Elite',
+      price: '1500',
+      list: [
+        { name: 'unlimited gym access' },
+        { name: 'all training programs' },
+        { name: 'free fitness consultation' },
+        { name: 'personal trainer' },
+        { name: '50% off drinks' },
+      ],
+      delay: 1000,
+    },
+  ],
+};
+
+export const join = {
+  image: JoinImg,
+  title: 'Wanna join & have fun?',
+  subtitle:
+    'We’ll keep you updated on the things you need to know about Gymme. Nothing more, nothing less.',
+  btnText: 'Join now',
+};
+
+export const footer = {
+  logo: Logo,
+  copyrightText: 'All rights reserved by Manwar',
+};
